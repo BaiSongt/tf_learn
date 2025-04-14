3196939a96331a91c13e2f61308fe99966f4f1fe
BaiSongtao <bst1367@outlook.com>
2025-04-14 10:32:39 +0800

    在 test_env.py 中添加 GPU 可用性检查和信息打印功能，以便更好地了解当前环境的 GPU 配置。

--- test_env.py
+++ test_env.py
@@ -4,3 +4,14 @@ print("Devices:", tf.config.list_physical_devices())
 
 import torch
 print("torch GPU: ", torch.cuda.is_available() )
+
+
+# 检查是否可以使用 GPU
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+# 打印 GPU 信息
+if tf.config.list_physical_devices('GPU'):
+    print("GPU is available")
+    print("GPU Device: ", tf.config.list_physical_devices('GPU'))
+else:
+    print("GPU is not available")
